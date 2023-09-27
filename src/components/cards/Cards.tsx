c9da5b43de780dc24840a5b76cf9fead27a3c8c7
Miguelkusanagui <99842468+Miguelkusanagui@users.noreply.github.com>
2023-09-27 12:35:52 -0600

    Card agregada

--- src/components/cards/Cards.tsx
+++ src/components/cards/Cards.tsx
@@ -1,11 +1,71 @@
+import React from 'react';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
 
-import React from 'react'
+const Cards = ({ info }) => {
+  const { name } = info;
 
-export default function Cards() {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <View style={styles.container}>
+      <View style={styles.cardContainer}>
+        <View style={styles.infoStyle}>
+          <Text style={styles.titleStyle}>{name}</Text>
 
+          <View style={styles.iconLabelStyle}>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const deviceWidth = Math.round(Dimensions.get('window').width);
+const offset = 40;
+const radius = 20;
+const styles = StyleSheet.create({
+  container: {
+    width: deviceWidth - 20,
+    alignItems: 'center',
+    marginTop: 25,
+  },
+  cardContainer: {
+    width: deviceWidth - offset,
+    backgroundColor: '#000',
+    height: 200,
+    borderRadius: radius,
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 5,
+      height: 5,
+    },
+    shadowOpacity: 0.75,
+    shadowRadius: 5,
+    elevation: 9,
+  },
+  imageStyle: {
+    height: 130,
+    width: deviceWidth - offset,
+    borderTopLeftRadius: radius,
+    borderTopRightRadius: radius,
+    opacity: 0.9,
+    alignContent: 'center',
+    alignSelf: 'center',
+  },
+  titleStyle: {
+    fontSize: 20,
+    fontWeight: '800',
+  },
+  categoryStyle: {
+    fontWeight: '200',
+  },
+  infoStyle: {
+    marginHorizontal: 10,
+    marginVertical: 5,
+  },
+  iconLabelStyle: {
+    flexDirection: 'row',
+    marginTop: 10,
+  },
+});
+
+export default Cards;

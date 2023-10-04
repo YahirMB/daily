54a6caa6ec0636716b7f2a237db55f618bac3b32
UrielHz <urielhz2002@gmail.com>
2023-10-03 19:04:01 -0600

    recoverAccount Uriel

---
+++ src/screen/recoverAccountScreen/RecoverAccountScreen1.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Text, View } from 'react-native';
+
+
+export const RecoverAccountScreen1 = () => {
+  return (
+    <View>
+        <Text> Hola mundo</Text>
+    </View>
+  )
+}

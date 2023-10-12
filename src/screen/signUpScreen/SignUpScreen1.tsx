4d863b8c6fa131825125cb8d2ff1574cca47433d
YahirMB <alexandermanjarrez18@gmail.com>
2023-10-12 15:12:57 -0600

    iniciando el envio de datos para SignUp

--- src/screen/signUpScreen/SignUpScreen1.tsx
+++ src/screen/signUpScreen/SignUpScreen1.tsx
@@ -1,5 +1,5 @@
 //#Libraries
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 //#Controls
 import { ButtonFilled } from '../../controls/buttonFilled/ButtonFilled';
@@ -14,23 +14,58 @@ import { imageStartCalendario } from '../../resources';
 //#Components
 import { LayoutScreen } from '../../layout/LayoutScreen';
 
+interface propStateSignUp {
+    name: string, 
+    lastName: string
+    [key:string] :string
+}
 
 
 export const SignUpScreen1 = ({ navigation }: any) => {
 
+    const [dataSignUp, setdataSignUp] = useState<propStateSignUp>({ name: '', lastName: '' })
+
     useEffect(() => {
         navigation.setOptions({
-          title: 'Crear cuenta',    
-      })
-      }, [])
-      
+            title: 'Crear cuenta',
+        })
+    }, [])
+
+    const onChangeValue = (valor: any, key: string) => {
+        setdataSignUp({ ...dataSignUp, [key]: valor });
+    }
+
+
+    const nextScreen = () => {
+
+        let AllFieldFille = true;
+
+        for (const key in dataSignUp) {
+            
+            if(dataSignUp[key] == ''){
+                AllFieldFille = false
+                console.log('esta vacio',key)
+            }
+        }
+
+        if(AllFieldFille){
+            console.log('va enviar la data y navegar a la proxima pagina')
+        }
+
+        // navigation.navigate('SignUp2')
+    }
+
     return (
 
         <LayoutScreen imgSrc={imageStartCalendario}>
 
             <Phrase>Necesitamos tus nombres y tus apellidos</Phrase>
-            <InputFilled nameLabel='Nombres'/>
-            <InputFilled nameLabel='Apellidos'/>
+            <InputFilled
+                event={e => onChangeValue(e, 'name')}
+                nameLabel='Nombres' />
+            <InputFilled
+                event={e => onChangeValue(e, 'lastName')}
+                nameLabel='Apellidos' />
 
             <BtnContainerSignUp1>
                 <ButtonFilled
@@ -40,7 +75,7 @@ export const SignUpScreen1 = ({ navigation }: any) => {
                     title='Tengo cuenta' />
                 <ButtonFilled
                     colorText='#32BC82'
-                    event={() => navigation.navigate('SignUp2')}
+                    event={nextScreen}
                     backgroundColor={'white'}
                     title='Siguiente' />
             </BtnContainerSignUp1>

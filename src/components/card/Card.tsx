99ae2d9638ae1d722c2789e93f2ddf4a4df1851f
YahirMB <alexandermanjarrez18@gmail.com>
2023-10-14 14:11:50 -0600

    screen de home cargadas

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,77 +1,78 @@
 import React from 'react'
 import { Dimensions, StyleSheet, Text, View } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 interface cardProps {
-    info: string
-    titleCard: string
-  
-  }
+  info: string
+  titleCard: string
 
-export const Card = ({ info, titleCard }: cardProps) => {
-    return (
-        <View style={{ padding: 20, left: 3, width: 50 }}>
-            <View style={styles.cardContainer}>
-                <View style={{ backgroundColor: '#32BC82', width: '100%', height: 'auto', borderTopLeftRadius: 20, borderTopRightRadius: 20, flexDirection: 'row', justifyContent: 'space-between', padding: 5 }}>
-                    <Text style={{ color: 'white', fontSize: 18 }}>
-                        5 min antes
-                    </Text>
-                    <Text style={{ color: 'white', fontSize: 18 }}>
-                        15:40hrs
-                    </Text>
-                </View>
-                <Text style={{ color: 'black', fontSize: 22, textAlign: 'auto', marginLeft: 15, marginVertical: 20 }}>
-                    {titleCard}
-                </Text>
-                <Text style={{ color: 'black', fontSize: 18, textAlign: 'auto', marginLeft: 15, marginVertical: 15 }}>
-                    {info}
-                </Text>
-                <View style={styles.infoStyle}>
-
-                </View>
-            </View>
-        </View>
-    )
 }
 
-const deviceWidth = Math.round(Dimensions.get('window').width);
-const offset = 40;
-const radius = 20;
+export const Card = ({ info, titleCard }: cardProps) => {
+  return (
+
+    <View style={styles.cardContainer}>
+      <View style={{ backgroundColor: '#32BC82', borderTopLeftRadius: 20, borderTopRightRadius: 20, flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal:10,paddingVertical:8 }}>
+        <Text style={{ color: 'white', fontSize: 18 }}>
+          5 min antes
+        </Text>
+
+        <View style={{ flexDirection: 'row', gap: 10 }}>
+
+          <Text style={{ color: 'white', fontSize: 18 }}>
+            15:40hrs
+          </Text>
+
+          <Icon name="ellipsis-vertical-sharp" color={'white'} size={25} />
+        </View>
+      </View>
+      <Text style={{ color: '#6D6B6B', fontSize: 20,fontWeight:'bold', textAlign: 'auto', margin:10}}>
+        {titleCard}
+      </Text>
+      <Text numberOfLines={2} ellipsizeMode='tail' style={{color: 'black', fontSize: 18, textAlign: 'auto', marginHorizontal:10 }}>
+        {info}
+      </Text>
+
+    </View>
+  )
+}
+
 
 const styles = StyleSheet.create({
-    container: {
-      width: deviceWidth - 20,
-      alignItems: 'center',
-      marginTop: 25,
-    },
-    cardContainer: {
-      width: deviceWidth - offset,
-      backgroundColor: '#D9D9D9',
-      height: 200,
-      borderRadius: 20,
-  
-    },
-  
-  info:{
+  container: {
+    width:'auto',
+    alignItems: 'center',
+    marginTop: 25,
+  },
+  cardContainer: {
+    width:"100%",
+    backgroundColor: '#D9D9D9',
+    height: 150,
+    borderRadius: 20,
+
+  },
+
+  info: {
     fontSize: 18,
     padding: 30,
-   
+
   },
-  
-  titleCard:{
-      marginLeft: 5,
-  
+
+  titleCard: {
+    marginLeft: 5,
+
   },
-    titleStyle: {
-      fontSize: 20,
-      fontWeight: '800',
-      color: 'black',
-    },
-    categoryStyle: {
-      fontWeight: '200',
-    },
-    infoStyle: {
-      marginHorizontal: 10,
-      marginVertical: 5,
-      color: 'black'
-    },
-  });
+  titleStyle: {
+    fontSize: 20,
+    fontWeight: '800',
+    color: 'black',
+  },
+  categoryStyle: {
+    fontWeight: '200',
+  },
+  infoStyle: {
+    marginHorizontal: 10,
+    marginVertical: 5,
+    color: 'black'
+  },
+});
--- src/screen/homeScreen/HomeScreen.tsx
+++ src/screen/homeScreen/HomeScreen.tsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useContext } from 'react'
-import { Button, FlatList, Text, View } from 'react-native'
+import { Button, FlatList, Text, View, ScrollView } from 'react-native'
 
 import Icon from 'react-native-vector-icons/Ionicons';
 import { NoteContext } from '../../context/NotesContext';
 import { Card } from '../../components/card/Card';
+import { CardsList } from '../../components/cardList/CardsList';
+
 
 export const HomeScreen = ({ navigation }: any) => {
 
   const { loadNotes, notes } = useContext(NoteContext)
 
-  const data = [1,2,3]
+  const data = [1, 2, 3]
 
   useEffect(() => {
 
@@ -23,35 +25,18 @@ export const HomeScreen = ({ navigation }: any) => {
   }, [])
 
 
-
-  console.log(notes)
-
-
-  interface note {
-    title: String
-  }
-  const ListNotes = ({ title }: note) =>
-
-  (<View>
-    <Text style={{ color: 'black' }}>{title}</Text>
-  </View>)
-
-
   return (
-    <View>
-      <Text>Hola inicio de la app</Text>
-
-      <Button title='regresar' onPress={() => navigation.navigate('Login')} />
-
-      {
-
-        data.map(card =>
-          <Card info='Sacar el perro de la cochera' titleCard='Cochera' />
-        )
+    <ScrollView>
 
 
-      }
+      <FlatList
+        style={{ marginTop: 40 }}
+        data={data}
+        renderItem={(item) => <CardsList data={data} />}
+        keyExtractor={(item, index) => index.toString()}
+      />
 
-    </View>
+
+    </ScrollView>
   )
 }

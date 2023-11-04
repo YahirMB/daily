e297a40b97cc9c0ccca06436d8dd97a5cf6abdda
UrielHz <urielhz2002@gmail.com>
2023-11-03 19:12:01 -0600

    Cambios screen edit profile

--- src/api/dailyplanApi.ts
+++ src/api/dailyplanApi.ts
@@ -2,13 +2,11 @@ import axios from "axios";
 
 
 
-const apiDaily = axios.create({ baseURL: 'http://192.168.1.6:3300/api/'});
+const apiDaily = axios.create({ baseURL: 'http://192.168.0.155:3300/api/'});
 // tplink-tics-7_5G
 //192.168.0.121
-//en ves de localhost va la ip de tu compu
-//para ver tu ip es tu terminal corres ipconfig
+//En vez de localhost va la ip de tu compu
+//para ver tu ip en tu terminal corres ipconfig
 
 
-
-
-export default apiDaily;
+export default apiDaily; 
